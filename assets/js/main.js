b4a0703e1a5e76de723d02007da208ade0c043ae
Maryam Saeedmehr <maryamsaeedmehr@gmail.com>
2022-10-11 14:12:50 +0330

    go-to-next only on mobiles

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,9 +105,10 @@
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
+  
 
   /**
-   * Scrool with ofset on links with a class name .scrollto
+   * Scroll with ofset on links with a class name .scrollto
    */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
