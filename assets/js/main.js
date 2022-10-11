fce279cf93b58ac4a89afa731d234a26693c6409
Maryam Saeedmehr <maryamsaeedmehr@gmail.com>
2022-10-11 12:45:53 +0330

    Color Palette + go-to-next button

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,6 +66,22 @@
   }
 
   /**
+   * Go to next button
+   */
+   let gotonext = select('.go-to-next')
+   if (gotonext) {
+     const toggleGoToNext = () => {
+       if (window.scrollY < 100) {
+         gotonext.classList.add('active')
+       } else {
+         gotonext.classList.remove('active')
+       }
+     }
+     window.addEventListener('load', toggleGoToNext)
+     onscroll(document, toggleGoToNext)
+   }
+
+  /**
    * Back to top button
    */
   let backtotop = select('.back-to-top')
@@ -128,6 +144,16 @@
       preloader.remove()
     });
   }
+
+  /**
+   * Leaddown
+   */
+   let leaddown = select('#leaddown');
+   if (leaddown) {
+     window.addEventListener('load', () => {
+       leaddown.remove()
+     });
+   }
 
   /**
    * Hero type effect
